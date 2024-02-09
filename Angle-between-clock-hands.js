f8942b4633dbadf4f03403da62a3b808bd7bd86b
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-08 19:58:13 -0800

    Update Angle-between-clock-hands.js

--- Angle-between-clock-hands.js
+++ Angle-between-clock-hands.js
@@ -28,3 +28,11 @@ function handAngle (date) {
 }
 
 // or
+
+function handAngle (date) {
+  const minutesAng = date.getMinutes() * 6;
+  const hoursAng = (date.getHours() * 30) + (date.getMinutes() * 0.5);
+  const angle = Math.abs(minutesAng - hoursAng);
+  const grad = Math.min(angle, 360 - angle);
+	return grad * Math.PI / 180;
+}
